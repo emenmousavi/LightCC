fe5a0234766c48707560d97cf109d5d87577c476
Amin Mousavi <108234550+emenmousavi@users.noreply.github.com>
2023-04-04 17:07:25 +0400

    Update lightcc.py

--- lightcc.py
+++ lightcc.py
@@ -1,33 +1,20 @@
 import random
-import requests
-import pycardvalidate
 from faker import Faker
+import pycardvalidate
+import requests
 
 def generate_credit_card_number():
-    """
-    Generates a random valid credit card number using the pycardvalidate library.
-    """
     while True:
         fake = Faker()
         credit_card_number = fake.credit_card_number(card_type=None)
         if pycardvalidate.validate(credit_card_number):
             return credit_card_number
 
-
 def validate_credit_card_number(credit_card_number):
-    """
-    Validates a credit card number using the https://binlist.io/ API.
-    """
-    # Make API call to binlist.io
     session = requests.Session()
     response = session.get(f'https://api.binlist.io/validate?format=json&credit_card_number={credit_card_number}')
-
-    # Check if API call was successful
     if response.status_code == 200:
-        # Parse JSON response
         data = response.json()
-
-        # Check if credit card is valid
         if 'valid' in data:
             return data
         else:
@@ -35,21 +22,11 @@ def validate_credit_card_number(credit_card_number):
     else:
         return {'error': f'Error: {response.status_code} - {response.reason}'}
 
-
 def get_credit_card_details(credit_card_number):
-    """
-    Gets details about a credit card using the https://binlist.io/ API.
-    """
-    # Make API call to binlist.io
     session = requests.Session()
     response = session.get(f'https://api.binlist.io/lookup/{credit_card_number}')
-
-    # Check if API call was successful
     if response.status_code == 200:
-        # Parse JSON response
         data = response.json()
-
-        # Build dictionary of credit card details
         credit_card_details = {}
         if 'bank' in data and 'numeric' in data['bank']:
             credit_card_details['issuing_bank_number'] = data['bank']['numeric']
@@ -61,19 +38,15 @@ def get_credit_card_details(credit_card_number):
             credit_card_details['card_level'] = data['type'].capitalize()
         if 'brand' in data:
             credit_card_details['card_type'] = data['brand'].capitalize()
-
         return credit_card_details
     else:
         return {'error': f'Error: {response.status_code} - {response.reason}'}
-
 
 def test_payment():
     card_number = input("Enter your credit card number: ")
     expiration_date = input("Enter your expiration date (MM/YY): ")
     cvv_code = input("Enter your CVV code: ")
     amount = "1"
-
-    # Make payment using u24.gov.ua site
     try:
         session = requests.Session()
         response = session.post('https://donate.u24.gov.ua/payment/create',
