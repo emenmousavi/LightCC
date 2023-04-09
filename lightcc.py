16ea1566f6bb215bcf26ba750cc4a5367d7bece8
Amin Mousavi <108234550+emenmousavi@users.noreply.github.com>
2023-04-09 12:15:11 +0400

    Update lightcc.py

--- lightcc.py
+++ lightcc.py
@@ -7,20 +7,29 @@ import os
 stripe.api_key = os.environ.get("STRIPE_API_KEY")
 stripe.api_version = "2020-08-27"
 
-# Function to generate a valid credit card number
+def main_menu():
+    print("Please choose an option:")
+    print("1. Generate a valid credit card number")
+    print("2. Validate a credit card number")
+    print("3. Get credit card details")
+    print("4. Process a payment using Stripe")
+    print("5. Exit")
+    choice = input("Enter your choice: ")
+    return choice
+
 def generate_credit_card_number():
     return pycardvalidator.generate()
 
-# Function to validate a credit card number
-def validate_credit_card_number(credit_card_number):
+def validate_credit_card_number():
+    credit_card_number = input("Enter the credit card number to validate: ")
     try:
         pycardvalidator.parseString(credit_card_number)
-        return True
+        print("Valid credit card number")
     except:
-        return False
+        print("Invalid credit card number")
 
-# Function to get credit card details
-def get_credit_card_details(credit_card_number):
+def get_credit_card_details():
+    credit_card_number = input("Enter the credit card number to get details: ")
     try:
         response = requests.get(f'https://lookup.binlist.net/{credit_card_number}')
         data = response.json()
@@ -35,12 +44,17 @@ def get_credit_card_details(credit_card_number):
             credit_card_details['card_level'] = data['type'].capitalize()
         if 'brand' in data:
             credit_card_details['card_type'] = data['brand'].capitalize()
-        return credit_card_details
+        print(credit_card_details)
     except:
-        return {'error': 'Unable to retrieve credit card details'}
+        print({'error': 'Unable to retrieve credit card details'})
 
-# Function to process a payment using Stripe
-def process_payment(card_number, exp_month, exp_year, cvc, amount):
+def process_payment():
+    card_number = input("Enter your credit card number: ")
+    expiration_date = input("Enter your expiration date (MM/YY): ")
+    exp_month, exp_year = expiration_date.split("/")
+    cvc_code = input("Enter your CVV code: ")
+    amount = input("Enter the payment amount: ")
+
     try:
         response = stripe.PaymentIntent.create(
             amount=amount,
@@ -51,32 +65,36 @@ def process_payment(card_number, exp_month, exp_year, cvc, amount):
                     "number": card_number,
                     "exp_month": exp_month,
                     "exp_year": exp_year,
-                    "cvc": cvc
+                    "cvc": cvc_code
                 }
             },
             confirm=True
         )
         if response.status == 'succeeded':
-            return {'status': 'success', 'message': 'Payment successful'}
+            print("Payment successful")
         else:
-            return {'status': 'error', 'message': 'Payment failed'}
+            print("Payment failed")
     except stripe.error.CardError as e:
         err = e.error
-        return {'status': 'error', 'message': err.message}
+        print(err.message)
     except Exception as e:
-        return {'status': 'error', 'message': str(e)}
+        print(str(e))
 
-# Function to test payment processing using Stripe
-def test_payment():
-    card_number = input("Enter your credit card number: ")
-    expiration_date = input("Enter your expiration date (MM/YY): ")
-    exp_month, exp_year = expiration_date.split("/")
-    cvc_code = input("Enter your CVV code: ")
-    amount = input("Enter the payment amount: ")
+def main():
+    while True:
+        choice = main_menu()
+        if choice == '1':
+            print(generate_credit_card_number())
+        elif choice == '2':
+            validate_credit_card_number()
+        elif choice == '3':
+            get_credit_card_details()
+        elif choice == '4':
+            process_payment()
+        elif choice == '5':
+            break
+        else:
+            print("Invalid choice, please try again")
 
-    response = process_payment(card_number, exp_month, exp_year, cvc_code, amount)
-
-    print(response['message'])
-
-# Test the payment function
-test_payment()
+if __name__ == '__main__':
+    main()
